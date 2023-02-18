085ee6f06a8b7c246afc18ce3c71baffa7ac211a
streaker303 <wangjian@threatbook.cn>
2023-02-18 23:01:22 +0800

    master分支

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,12 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h2>Master分支</h2>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: 'App'
 }
 </script>
 
